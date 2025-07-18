4ea1f6e225ac3ecb5cf3ef925cc4e1fd4414d94f
pijo2 <81983838+pijo2@users.noreply.github.com>
2025-07-18 08:54:31 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,7 +51,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
-                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                            <p>{{ score(selected + 1, 38, level.percentToQualify) }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
