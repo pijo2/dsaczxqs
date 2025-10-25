f601d38c8c0530153cf0d0b48434b5ad9694616c
pijo2 <81983838+pijo2@users.noreply.github.com>
2025-10-25 21:11:04 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import sperma from './pages/sperma.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/sperma', component: sperma },
 ];
