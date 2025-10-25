6aa460e2d19dad79691145b71f9dcd5613a0da4a
pijo2 <81983838+pijo2@users.noreply.github.com>
2025-10-25 20:58:57 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -6,4 +6,5 @@ export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/sperma', component: sperma },
 ];
