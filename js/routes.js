3ae45703b63b774616db5e212badffb37ec5f3ec
pijo2 <81983838+pijo2@users.noreply.github.com>
2025-11-15 11:37:05 +0100

    Update import path for Packs component

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import Packs from './pages/Roulette.js';
+import Packs from './pages/packs.js';
 
 export default [
     { path: '/', component: List },
